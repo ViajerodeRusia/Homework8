f052e2df259870f95df3dfb030cf6cbabd559d29
Denis Siliukov <abhem21rus@mail.ru>
2023-12-05 11:12:08 +0300

    Task completed

---
+++ src/Hufflepuff.java
@@ -0,0 +1,50 @@
+public class Hufflepuff extends Hogwarts {
+    private int diligence;
+    private int loyalty;
+    private int honesty;
+    public Hufflepuff(String name,String surname, int transgression, int witchcraft, int diligence,
+                      int loyalty, int honesty) {
+        super(name, surname, transgression, witchcraft);
+        this.diligence = diligence;
+        this.loyalty = loyalty;
+        this.honesty = honesty;
+    }
+    public static void compareHogwartsHouse(Hufflepuff[] hufflepuffs,int first, int second){
+        int sum1 = hufflepuffs[first].getDiligence()+hufflepuffs[first].getLoyalty()+hufflepuffs[first].getHonesty();
+        int sum2 = hufflepuffs[second].getDiligence()+hufflepuffs[second].getLoyalty()+hufflepuffs[second].getHonesty();
+        System.out.println("Comparison of two Hufflepuff students:");
+        if (sum1>sum2) {
+            System.out.println("Total "+sum1+ " (" + hufflepuffs[first].getName() + " " + hufflepuffs[first].getSurname() + ")" + " > " +
+                    "Total "+sum2+ " (" + hufflepuffs[second].getName() + " " + hufflepuffs[second].getSurname() + ")");
+        } else if (sum1<sum2) {
+            System.out.println("Total "+sum1+ " (" + hufflepuffs[first].getName() + " " + hufflepuffs[first].getSurname() + ")" + " < " +
+                    "Total "+sum2+ " (" + hufflepuffs[second].getName() + " " + hufflepuffs[second].getSurname() + ")");
+        } else if (sum1==sum2) {
+            System.out.println("Total "+sum1+ " (" + hufflepuffs[first].getName() + " " + hufflepuffs[first].getSurname() + ")" + " = " +
+                    "Total "+sum2+ " (" + hufflepuffs[second].getName() + " " + hufflepuffs[second].getSurname() + ")");
+        }
+        System.out.println("**************");
+    }
+
+    @Override
+    public String toString() {
+        return "Hufflepuff student \n" + "Name: " + getName() + " Surname: " + getSurname() + "\n" +
+                "Transgression=" + getTransgression() + "\n" +
+                "Witchcraft=" + getWitchcraft() + "\n" +
+                "Diligence=" + diligence + "\n" +
+                "Loyalty=" + loyalty + "\n" +
+                "Honesty=" + honesty + "\n";
+    }
+
+    public int getDiligence() {
+        return diligence;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
+
+    public int getHonesty() {
+        return honesty;
+    }
+}
